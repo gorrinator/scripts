94e0ba330ac585c1028598e87a746e3fe076aa85
gorrinator <42017094+gorrinator@users.noreply.github.com>
2018-08-02 12:37:45 +1000

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,4 +1,5 @@
+#Script to convert an XLS sitting in a specific directory to a CSV file in a pre-defined path
 import pandas as pd
-df = pd.read_excel(r'C:\Users\bag573\Desktop\301_redirect.xls',encoding = "ISO-8859-1")
-df.to_csv(r'C:\Users\bag573\Desktop\301_redirect.csv',index=False,encoding = "ISO-8859-1")
+df = pd.read_excel(r'#insert file path',encoding = "ISO-8859-1")
+df.to_csv(r'#insert output path',index=False,encoding = "ISO-8859-1")
 print('File Successfully Converted!')
