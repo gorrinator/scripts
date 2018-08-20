cce2c963bcf65f28274864d82d0650f57b9ffa23
gorrinator <42017094+gorrinator@users.noreply.github.com>
2018-08-20 14:10:54 +1000

    Update cv.py

--- cv.py
+++ cv.py
@@ -1,10 +1,10 @@
-#Script that will convert the selected XLS file to a CSV named 'File' to C:\
+# Script that will convert the selected XLS file to a CSV named 'File' to C:\
 import pandas as pd
 import tkinter as tk
 from tkinter import filedialog
 root = tk.Tk()
 root.withdraw()
-file_path = filedialog.askopenfilename() 
+file_path = filedialog.askopenfilename() # dialog box to select the XLS file you would like to convert.
 df = pd.read_excel('%s'%(file_path))
-df.to_csv('C:\\file.csv')
-print('File Successfully Converted!')
+df.to_csv('C:\\file.csv') # pre-defined path where you would like the converted file to be saved.
+print('File Successfully Converted!') # message to advise the script has completed.
