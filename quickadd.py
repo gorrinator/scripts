b028edb16df5ea0fe59f4664e6599a4482ec4b7e
gorrinator <42017094+gorrinator@users.noreply.github.com>
2018-09-04 13:28:33 +1000

    Update quickadd.py

--- quickadd.py
+++ quickadd.py
@@ -7,6 +7,8 @@ from oauth2client import file, client, tools
 SCOPES = 'https://www.googleapis.com/auth/calendar'
 import sys
 time = str(sys.argv[1])
+date = str(sys.argv[2])
+input = time + date
 def main():
     store = file.Storage('token.json')
     creds = store.get()
@@ -16,7 +18,7 @@ def main():
     service = build('calendar', 'v3', http=creds.authorize(Http()))
     created_event = service.events().quickAdd(
     calendarId='neto.com.au_t6v4op72aj7d279hhbacq1h32o@group.calendar.google.com',
-    text='%s Go Lives'%time).execute()
+    text='%s %s Go Lives'%(time,date)).execute()
 
     print (created_event['id'])
 if __name__ == '__main__':
