731a04bc9939bf53ae9db01db0158f513bb30150
gorrinator <42017094+gorrinator@users.noreply.github.com>
2018-09-04 13:35:43 +1000

    Update quickadd.py

--- quickadd.py
+++ quickadd.py
@@ -8,7 +8,7 @@ SCOPES = 'https://www.googleapis.com/auth/calendar'
 import sys
 time = str(sys.argv[1])
 date = str(sys.argv[2])
-input = time + date
+desc = str(sys.argv[3])
 def main():
     store = file.Storage('token.json')
     creds = store.get()
@@ -18,7 +18,7 @@ def main():
     service = build('calendar', 'v3', http=creds.authorize(Http()))
     created_event = service.events().quickAdd(
     calendarId='neto.com.au_t6v4op72aj7d279hhbacq1h32o@group.calendar.google.com',
-    text='%s %s Go Lives'%(time,date)).execute()
+    text='%s %s %s'%(time,date,desc)).execute()
 
     print (created_event['id'])
 if __name__ == '__main__':
